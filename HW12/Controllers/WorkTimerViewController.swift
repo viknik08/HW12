759fd8864330bb4f0e8419bc07d3792b223b6c43
viknik08 <63363344+viknik08@users.noreply.github.com>
2022-08-19 16:01:55 +0800

    add outlets + MARK

--- HW12/Controllers/WorkTimerViewController.swift
+++ HW12/Controllers/WorkTimerViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class WorkTimerViewController: UIViewController {
+//MARK: - Outlets
+    private let timerLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 50)
+        label.text = "1"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let timerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        button.addTarget(self, action: #selector(timerButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+//MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    }
+    
+//MARK: - Setup
+    
+
+//MARK: - Action
+    @objc func timerButtonTapped() {
+        timerButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+
     }
 
-
 }
+
+
 
